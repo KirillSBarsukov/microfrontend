63d1abaca4beadd1c9c35a9e0e4f8c4dd6e95e0b
Kirill Barsukov <kirillsbarsukov@gmail.com>
2021-12-09 17:33:50 -0500

    fixed css

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import  {BrowserRouter} from "react-router-dom"
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from "./components/Header";
 
+const packageJson = require('../package.json');
+
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: packageJson.name
+})
 
 export default () => {
   return(
     <BrowserRouter>
-        <div>
-            <Header/>
-            <MarketingApp />
-        </div>
+        <StylesProvider generateClassName={generateClassName}>
+            <div>
+                <Header/>
+                <MarketingApp />
+            </div>
+        </StylesProvider>
     </BrowserRouter>
   );
 };
